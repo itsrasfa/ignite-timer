e5166f64d37e1c0b75a9085d9aa01ffbb87fcc30
itsrasfa <rafaelacontatoss@gmail.com>
2022-09-04 03:47:47 -0300

    feat: create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
+import * as S from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+import logoIgnite from '../../../public/Logo Ignite.svg'
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <S.HeaderContainer>
+      <img src={logoIgnite} alt="" />
+
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </S.HeaderContainer>
+  )
 }
