ca362f6b4575d2e9d43532a919d4f429884ec369
itsrasfa <rafaelacontatoss@gmail.com>
2023-03-06 00:34:00 -0300

    feat: list timer history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { CyclesContext } from '../../contexts/CyclesContext'
 import * as S from './styles'
 
 export function History() {
+  const { cycles } = useContext(CyclesContext)
   return (
     <S.HistoryContainer>
       <h1>Meu histórico</h1>
@@ -15,14 +20,32 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 Minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <S.Status statusColor="red">Em andamento</S.Status>
-              </td>
-            </tr>
+            {cycles?.map((cycle) => {
+              const formattedDate = new Date(cycle.startDate)
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>
+                    {formatDistanceToNow(formattedDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
+                  <td>
+                    {cycle.finishedDate && (
+                      <S.Status statusColor="green">Concluído</S.Status>
+                    )}
+                    {cycle.interruptedDate && (
+                      <S.Status statusColor="red">Interrompido</S.Status>
+                    )}
+                    {!cycle.interruptedDate && !cycle.finishedDate && (
+                      <S.Status statusColor="yellow">Em andamento</S.Status>
+                    )}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </S.HistoryList>
